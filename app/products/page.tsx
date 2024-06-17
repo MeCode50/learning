4a2cccd9cc7abc3cbd258c0f4316c1d238ae56ce
Munachimso <Victorycollins31@gmail.com>
2024-06-17 13:38:05 +0100

    understood nested routing

---
+++ app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+    return (
+      <>
+        <h1>Product List</h1>
+        <h2>Sanitary pad</h2>
+        <h2>Acoustic guitar</h2>
+        <h2>Wrist watch</h2>
+      </>
+    );
+}
